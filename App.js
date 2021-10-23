5ababf41aad9e001c394ba1accc94d5d9749ca1e
gabrielcoelhoo <66799435+gabrielcoelhoo@users.noreply.github.com>
2021-10-23 13:00:15 +0100

    Implementation of 2 screens

--- App.js
+++ App.js
@@ -1,112 +1,100 @@
-/**
- Summary
-
- import
- functions
- useEffect
- permissions
- const for startRecording, stopRecording, playSound and stopSound
- view
- style
- */
-
-//import components to be used and camera and video functionalities
-
 import React, { useState, useRef, useEffect } from 'react';
-import { View, StyleSheet, Button, Text } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Audio } from 'expo-av';
 
 let recording = new Audio.Recording();
 
-//set the useStates as false which will be changed when start an action 
 export default function App() {
-  const [RecordedURI, SetRecordedURI] = useState('');
+
+const [current, setCurrent] = useState('Home');
+const [RecordedURI, SetRecordedURI] = useState('');
   const [AudioPerm, SetAudioPerm] = useState(false);
   const [isRecording, SetisRecording] = useState(false);
   const [isPLaying, SetisPLaying] = useState(false);
-
   //react hooks to play a new audio
-  const Player = useRef(new Audio.Sound());
+const Player = useRef(new Audio.Sound());
 
-  useEffect(() => {
-    GetPermission();
-  }, []);
+useEffect(() => {
+  GetPermission();
+}, []);
 
-  //audio permission
-  const GetPermission = async () => {
-    const getAudioPerm = await Audio.requestPermissionsAsync();
-    SetAudioPerm(getAudioPerm.granted);
-  };
+//audio permission
+const GetPermission = async () => {
+  const getAudioPerm = await Audio.requestPermissionsAsync();
+  SetAudioPerm(getAudioPerm.granted);
+};
 
-  //actions of start and stop audio and recordings
+//actions of start and stop audio and recordings
 
-  const startRecording = async () => {
-    if (AudioPerm === true) {
-      try {
-        await recording.prepareToRecordAsync(
-          Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
-        );
-        await recording.startAsync();
-        SetisRecording(true);
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      GetPermission();
-    }
-  };
-
-  const stopRecording = async () => {
+const startRecording = async () => {
+  if (AudioPerm === true) {
     try {
-      await recording.stopAndUnloadAsync();
-      const result = recording.getURI();
-      SetRecordedURI(result); // Here is the URI
-      recording = new Audio.Recording();
-      SetisRecording(false);
+      await recording.prepareToRecordAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      await recording.startAsync();
+      SetisRecording(true);
     } catch (error) {
       console.log(error);
     }
-  };
+  } else {
+    GetPermission();
+  }
+};
+
+const stopRecording = async () => {
+  try {
+    await recording.stopAndUnloadAsync();
+    const result = recording.getURI();
+    SetRecordedURI(result); // Here is the URI
+    recording = new Audio.Recording();
+    SetisRecording(false);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //playsound - code to execute a file after the recording 
 //reference 
 //https://stackoverflow.com/questions/67207450/how-can-i-play-a-sound-after-recording-in-react-native-expo
-  
+
 const playSound = async () => {
-    try {
-      const result = await Player.current.loadAsync(
-        { uri: RecordedURI }, // RecordedURI - this is the parameter where is allocated the audio file
-        {},
-        true
-      );
+  try {
+    const result = await Player.current.loadAsync(
+      { uri: RecordedURI }, // RecordedURI - this is the parameter where is allocated the audio file
+      {},
+      true
+    );
 
-      const response = await Player.current.getStatusAsync();
-      if (response.isLoaded) {
-        if (response.isPlaying === false) {
-          Player.current.playAsync();
-          SetisPLaying(true);
-        }
+    const response = await Player.current.getStatusAsync();
+    if (response.isLoaded) {
+      if (response.isPlaying === false) {
+        Player.current.playAsync();
+        SetisPLaying(true);
       }
-    } catch (error) {
-      console.log(error);
     }
-  };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
- const stopSound = async () => {
-    try {
-      const checkLoading = await Player.current.getStatusAsync();
-      if (checkLoading.isLoaded === true) {
-        await Player.current.stopAsync();
-        SetisPLaying(false);
-      }
-    } catch (error) {
-      console.log(error);
+const stopSound = async () => {
+  try {
+    const checkLoading = await Player.current.getStatusAsync();
+    if (checkLoading.isLoaded === true) {
+      await Player.current.stopAsync();
+      SetisPLaying(false);
     }
-  };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  return (
-    <View style={styles.container}>
-      <Button
+const HomeScreen = (
+<View style={styles.container}>
+<Button
         title={isRecording ? 'Stop Recording' : 'Start Recording'}
         onPress={isRecording ? () => stopRecording() : () => startRecording()}
       />
@@ -114,19 +102,34 @@ const playSound = async () => {
         title="Play Sound"
         onPress={isPLaying ? () => stopSound : () => playSound()} 
       />
-    </View>
-  );
-}
+<Text>(Screen 1)</Text>
+<Text>Welcome to My App</Text>
+<Button
+title="Start"
+onPress={() => setCurrent(DashBoardScreen)} 
+></Button>
+</View>
+);
 
+const DashBoardScreen = (
+  <View style={styles.container}>
+  <Text>(Screen 2)</Text>
+  <Text>DashBoard</Text>
+  <Button
+  title="Back"
+  onPress={() => setCurrent(HomeScreen)} 
+  ></Button>
+  </View>
+);
+
+return current === 'Home' ? HomeScreen : current;
+
+}
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+
+   marginTop: 200,
   },
 });
 
-//reference
-//recording sound code
-//https://docs.expo.dev/versions/latest/sdk/audio/
+
